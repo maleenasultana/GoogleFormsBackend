b0f52c09d13b7361b2923ff8aef607401c657dd1
Maleena <maleena.sohail@gmail.com>
2023-08-05 18:57:55 +0530

    ch

--- db/config.js
+++ db/config.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
 
 const connectdb = () => {
-    mongoose.connect('mongodb://localhost:27017/googleForms', (error) => {
-    if (error) console.log('error in connecting db', error);
-    else console.log('db connected');
-});
+    mongoose.connect('mongodb://localhost:27017/googleForms',  {
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+    
+})
+
+.then(db => console.log('DB is connected'))
+.catch(err => console.log(err));
+
 }
 
 module.exports = connectdb;
